ee5b0257911a931cbcff373bb1e472aff7fa6c47
DarkenedDragon <eridor314@gmail.com>
2021-03-05 16:52:05 -0500

    Created the ClassModel, which stores information about each class in SIS

--- map/models.py
+++ map/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class ClassModel(models.Model):
+    class_number = models.IntegerField()  # This is the 5 digit unique class ID. Ex: 15927
+    class_mnemonic = models.CharField()
+    course_number = models.IntegerField()  # This is the 4 digit course number, but is not specific to section Ex: 3240
+    class_section = models.IntegerField()
+    class_type = models.CharField()
+    class_units = models.IntegerField()
+    class_instructor = models.CharField()
+    class_days = models.DateTimeField()
+    class_room = models.CharField()
+    class_title = models.CharField()
+    class_topic = models.CharField()
+    class_status = models.CharField()
+    class_enrollment = models.IntegerField()
+    class_enrollment_limit = models.IntegerField()
+    class_waitlist = models.IntegerField()
+    # TODO: Make this refer to an instance of ClassModel
+    # class_combined_with = models.ForeignKey(object.__class__)
+    class_description = models.CharField()
+
+    def __str__(self):
+        return str(self.class_number)
+
--- map/views.py
+++ map/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
-def testMap(request):
-    return HttpResponse("Hello World.");
+def test_map(request):
+
+
+    return HttpResponse("Hello World.")
