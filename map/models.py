052aea31c6ad7c5cdcb8a360eaf4a20de2a2f986
DarkenedDragon <eridor314@gmail.com>
2021-03-09 15:58:09 -0500

    update_classes() now updates the ClassModel table in the database

--- map/models.py
+++ map/models.py
@@ -10,9 +10,9 @@ class ClassModel(models.Model):
     course_number = models.IntegerField()  # This is the 4 digit course number, but is not specific to section Ex: 3240
     class_section = models.IntegerField()
     class_type = models.CharField(max_length=200)
-    class_units = models.IntegerField()
+    class_units = models.FloatField()
     class_instructor = models.CharField(max_length=200)
-    class_days = models.DateTimeField()
+    class_days = models.CharField(max_length=100)  # TODO: This will need to be more complicated to store up to 3 durations
     class_room = models.CharField(max_length=200)
     class_title = models.CharField(max_length=200)
     class_topic = models.CharField(max_length=200)
@@ -22,7 +22,7 @@ class ClassModel(models.Model):
     class_waitlist = models.IntegerField()
     # TODO: Make this refer to an instance of ClassModel
     # class_combined_with = models.ForeignKey(object.__class__)
-    class_description = models.CharField(max_length=200)
+    class_description = models.CharField(max_length=2000)
 
     def __str__(self):
         return str(self.class_number)
--- map/signals.py
+++ map/signals.py
@@ -6,28 +6,48 @@ import csv
 
 @receiver(connection_created)
 def update_classes(sender, **kwargs):
-    """ Called when the database is first connected. Moves all the data in the
+    """
+    Called when the database is first connected. Moves all the data in the
     csv into the database if it isn't there already
     """
 
     print("Updating SIS data")
     # If there isn't any data in the table we need to update it
+    # TODO: Change this to if the table is not as long as the .csv file, but make it efficient...
     if not ClassModel.objects.all().exists():
+        # Value to count how many were updated
+        # So there is more to log
+        num_updated = 0
         # Read in the csv.
         with open(file='map/static/map/searchData.csv', newline='') as sis_data:
             reader = csv.reader(sis_data, delimiter=',')
             reader.__next__()  # Move past the header line
             for row in reader:
+                # TODO: Change these if it means something important
+                # For some reason graduate studies seem to have dates coded instead of units
+                # So just make them 0 units?
+                units = row[5]
+                if str(units).__contains__('/') or str(units).__contains__('-'):
+                    units = 0
+
+                # Similarly, some sections are labeled "ROS" or "CHO"
+                # No idea what it means, but messes up our table >:(
+                section = row[3]
+                try:
+                    section = int(section)
+                except ValueError:
+                    section = -1
+
                 # create the database entry
                 entry = ClassModel(
                     class_number=row[0],  # This is the 5 digit unique class ID. Ex: 15927
                     class_mnemonic=row[1],
                     course_number=row[2],  # This is the 4 digit course number, but is not specific to section Ex: 3240
-                    class_section=row[3],
+                    class_section=section,
                     class_type=row[4],
-                    class_units=row[5],
+                    class_units=units,
                     class_instructor=row[6],
-                    class_days=row[7],
+                    class_days=row[7],  # TODO: Fix this thing's formatting
                     class_room=row[8],
                     class_title=row[9],
                     class_topic=row[10],
@@ -40,6 +60,8 @@ def update_classes(sender, **kwargs):
                 )
                 # Add the row to the database
                 entry.save()
+                num_updated += 1
+            print("Updated %i entries" % num_updated)
     # The data is already in the database
     else:
         print("SIS data up to date")
