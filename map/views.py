5d2008f6eb162137ceec09d5dec43c1d71226c8b
DarkenedDragon <eridor314@gmail.com>
2021-03-17 14:43:39 -0400

    Added search function to the map

--- map/views.py
+++ map/views.py
@@ -6,3 +6,12 @@ from django.views import generic
 
 class MapView(generic.TemplateView):
     template_name = "map.html"
+
+    starting_coords = [-78.510067, 38.038124]
+
+    # From : https://stackoverflow.com/questions/18232851/django-passing-variables-to-templates-from-class-based-views
+    # Makes this classes global variables accessible from the templates
+    def get_context_data(self, **kwargs):
+        context = super(MapView, self).get_context_data(**kwargs)
+        context.update({'starting_coords': self.starting_coords})
+        return context
