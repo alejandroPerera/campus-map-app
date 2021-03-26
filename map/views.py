d8db75a69533a9d907b8b1fb9be67e0b7d89ab29
DarkenedDragon <eridor314@gmail.com>
2021-03-25 22:09:30 -0400

    Added forward GeoCode lookup

--- map/views.py
+++ map/views.py
@@ -1,19 +1,68 @@
 from django.http import HttpResponse
 from django.views import generic
+import requests
+import json
 
 
 # Create your views here.
+
+class GeoCode:
+    """
+    Data format:
+    {
+    type: ...,
+    query: [What you searched for],
+    features: [{lots of data}]
+    attribution: copyright notice. This data cannot be retained?
+    }
+    """
+
+    def __init__(self, response):
+        data = json.load(response)
+        self.query = data.get('query')
+        self.coordinates = (data.get('features')[0].get('center')[0], data.get('features')[0].get('center')[1])
+        self.name = data.get('features')[0].get('text')
+
+    def __init__(self, query, coordinates, name):
+        self.query = query
+        self.coordinates = coordinates
+        self.name = name
+
+    @staticmethod
+    def get_geo_codes(response):
+        """ Transforms the json data into an array of GeoCodes """
+        data = json.load(response)
+        query = ' '.join(data.get('query'))
+        results = data.get('features')
+        output = []
+        for result in results:
+            coordinates = (result.get('center')[0], result.get('center')[1])
+            name = result.get('text')
+            output.append(GeoCode(query, coordinates, name))
+
+        return output
+
 
 class MapView(generic.TemplateView):
     template_name = "map.html"
 
     starting_coords = [-78.510067, 38.038124]
+    # TODO: Make this an env variable
+    access_token = 'pk.eyJ1IjoiYS0wMiIsImEiOiJja21iMzl4dHgxeHFtMnBxc285NGMwZG5kIn0.Rl2qXrod77iHqUJ-eMbkcg'
 
-    # TODO: Move access token and other sensitive things here
+    def search_for_location(self, query) -> [GeoCode]:
+        base_url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'
+        params = {'limit': 5,
+                  'proximity': str(self.starting_coords[0]) + ',' + str(self.starting_coords[1]),
+                  'access_token': self.access_token}
+        # Get the data from MapBox's API
+        r = requests.get(base_url + query + '.json', params=params)
+        # Parse that data into a more useful form
+        return GeoCode.get_geo_codes(r.json())
 
     # From : https://stackoverflow.com/questions/18232851/django-passing-variables-to-templates-from-class-based-views
     # Makes this classes global variables accessible from the templates
     def get_context_data(self, **kwargs):
         context = super(MapView, self).get_context_data(**kwargs)
-        context.update({'starting_coords': self.starting_coords})
+        context.update({'starting_coords': self.starting_coords, 'access_token': self.access_token})
         return context
