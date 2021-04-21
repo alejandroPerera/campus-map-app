d26e7b82cced96119255ae547c7a478d215d820b
zacharyyahn <zry2yz@virginia.edu>
2021-04-20 22:16:30 -0400

    Fixed timezone issues

--- map/templatetags/check_date.py
+++ map/templatetags/check_date.py
@@ -1,13 +1,19 @@
 from django import template
 import datetime
-import pytz
+from pytz import timezone
 
 #Something for the custom filter for searching through a queryset
 register = template.Library()
-utc=pytz.UTC
+eastern = timezone('US/Eastern')
+central = timezone('US/Central')
+
 
 @register.filter
 def check_date(value):
-    value = value.replace(tzinfo=utc)
-    return value >= datetime.datetime.now().replace(tzinfo=utc)
+    #value = value.replace(tzinfo=utc)
+    now = datetime.datetime.now().replace(tzinfo=eastern)
+    value = value.astimezone(central)
+    print("Value: " + str(value) + "Now: " + str(now))
+    print(value >= now)
+    return value >= now
 
