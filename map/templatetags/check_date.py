9544711fbef46c2537e9c1b29c8511c8f38ee6a5
dyc2fgh <dyc2fgh@virginia.edu>
2021-05-03 22:00:11 -0400

    fixed check date bug

--- map/templatetags/check_date.py
+++ map/templatetags/check_date.py
@@ -8,17 +8,25 @@ register = template.Library()
 
 
 @register.filter
-def check_date(value):
+def check_date(event):
     #value = value.replace(tzinfo=utc)
-    now = date.today()
-    print("Value: " + str(value) + "Now: " + str(now))
-    print(value >= now)
-    return value >= now
+    currentDay = date.today()
+    print("Value: " + str(event.date) + "Now: " + str(currentDay))
+    print(event.date >= currentDay)
+    currentTime = datetime.now().time()
+    print("Value: " + str(event.time) + "Now: " + str(currentTime))
+    print(event.time >= currentTime)
+    if(event.date>currentDay):
+        return event.date > currentDay
+    elif(event.date == currentDay):
+        return event.time >= currentTime
+    else:
+        return False
 
-@register.filter
-def check_time(value):
-    now = datetime.now().time()
-    print("Value: " + str(value) + "Now: " + str(now))
-    print(value >= now)
-    return value >=now
+# @register.filter
+# def check_time(value):
+#     now = datetime.now().time()
+#     print("Value: " + str(value) + "Now: " + str(now))
+#     print(value >= now)
+#     return value >=now
 
